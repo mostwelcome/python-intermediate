5a0809b6a355ad924efad7e6f27e3e5c9bfa900c
most_welcome <tuadutta13@gmail.com>
2021-10-01 23:25:59 +0530

    changed format

--- oneday/fibonacci.py
+++ oneday/fibonacci.py
@@ -1,6 +1,7 @@
-'''
+"""
 0 1 1 2 3 5 8 ....
-'''
+
+"""
 i = int(input('Enter number'))
 
 
@@ -8,7 +9,7 @@ def fibo(n):
     if n <= 1:
         return n
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo(n - 1) + fibo(n - 2)
 
 
 for j in range(i):
