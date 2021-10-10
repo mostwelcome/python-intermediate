b9f43483a17d91ce55fad60c485d324c0b441534
most_welcome <tuadutta13@gmail.com>
2021-10-10 11:56:33 +0530

    tried new way

---
+++ oop/class1.py
@@ -0,0 +1,17 @@
+class Test:
+    def __init__(self, name):
+        self.name = name
+
+    def __init__(self, title, name):
+        self.title = title
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name} {self.title}'
+
+
+t1 = Test('swag')
+t2 = Test('developer', 'swag')
+
+print(t1)
+print(t2)
