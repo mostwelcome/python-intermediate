5a345033ad831f9522a0f7f1d26de8ddcb2b9b35
most_welcome <tuadutta13@gmail.com>
2021-10-14 22:33:20 +0530

    added multiple inheritance

---
+++ oop/multiple_inheritance.py
@@ -0,0 +1,27 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Understanding multiple inheritance
+
+
+class A:
+    def __init__(self):
+        super().__init__()
+        self.foo = "foo"
+
+
+class B:
+    def __init__(self):
+        super().__init__()
+        self.bar = "bar"
+
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+
+    def show_props(self):
+        print(self.foo)
+        print(self.bar)
+
+
+c = C()
+c.show_props()
